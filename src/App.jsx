9f993e2ebb680774cabfd1ab7aae6f4824766ea8
romanfaria <roman.faria@gmail.com>
2023-07-11 00:24:07 -0400

    got single user selection working committing before I mess anything else up

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
 import Calendar from "./scenes/calendar/calendar";
+import UserDetails from "./scenes/UserDetails/UserDetails";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -31,6 +32,7 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
+              <Route path="/team/user/:userId" element={<UserDetails />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
